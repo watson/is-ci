9b6cf26f14af01c8298e03aeefcdbeae37a5c0a4
Thomas Watson Steen <w@tson.dk>
2015-10-08 12:46:59 +0200

    Add TeamCity detection

--- index.js
+++ index.js
@@ -5,5 +5,6 @@ module.exports = !!(
   process.env.CONTINUOUS_INTEGRATION ||
   process.env.JENKINS_URL ||
   process.env.bamboo_planKey ||
-  process.env.TF_BUILD
+  process.env.TF_BUILD ||
+  process.env.TEAMCITY_VERSION
 )
