6d14b7cc21563a43969eefa0c52be9754ed0fec4
Bjarke Walling <bwp@bwp.dk>
2016-06-06 10:49:28 +0200

    Add Bitbucket Pipelines

--- index.js
+++ index.js
@@ -15,5 +15,6 @@ module.exports = !!(
   process.env.HUDSON_URL ||                      // Hudson
   (process.env.TASK_ID && process.env.RUN_ID) || // TaskCluster
   process.env.GO_PIPELINE_LABEL ||               // GoCD
+  process.env.BITBUCKET_COMMIT ||                // Bitbucket Pipelines
   false
 )
