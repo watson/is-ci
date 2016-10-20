483e71c989b7bf295d0c7fa1665683633fb64f73
Thomas Watson Steen <w@tson.dk>
2016-10-20 18:38:03 +0200

    Fix Travis testing issues

--- test.js
+++ test.js
@@ -3,13 +3,15 @@
 var assert = require('assert')
 var clearRequire = require('clear-require')
 
-if (!process.env.TRAVIS) process.env.CI = 'true'
+process.env.CI = 'true'
 
 var isCI = require('./')
 assert(isCI)
 
 delete process.env.CI
 delete process.env.CONTINUOUS_INTEGRATION
+delete process.env.BUILD_NUMBER
+delete process.env.TRAVIS
 
 clearRequire('./')
 clearRequire('ci-info')
