a5374eecdfc13248810c4b5ba1b6123b2699e6a3
Thomas Watson <w@tson.dk>
2018-12-05 22:35:39 +0100

    chore(package): upgrade dev-dependencies

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 'use strict'
 
 var assert = require('assert')
-var clearRequire = require('clear-require')
+var clearModule = require('clear-module')
 
 process.env.CI = 'true'
 
@@ -13,7 +13,7 @@ delete process.env.CONTINUOUS_INTEGRATION
 delete process.env.BUILD_NUMBER
 delete process.env.TRAVIS
 
-clearRequire('./')
-clearRequire('ci-info')
+clearModule('./')
+clearModule('ci-info')
 isCI = require('./')
 assert(!isCI)
