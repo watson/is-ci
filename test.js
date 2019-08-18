a4ca37dfbdbcc8ed9365c51e0e7807effbd42898
Jakob Krigovsky <jakob@krigovsky.com>
2019-08-18 13:17:44 +0200

    Use const and let instead of var

--- test.js
+++ test.js
@@ -1,11 +1,11 @@
 'use strict'
 
-var assert = require('assert')
-var clearModule = require('clear-module')
+const assert = require('assert')
+const clearModule = require('clear-module')
 
 process.env.CI = 'true'
 
-var isCI = require('./')
+let isCI = require('./')
 assert(isCI)
 
 delete process.env.CI
